5f87afd1acbeb4f5264633c0ddf0bd57f124bca2
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2020-09-02 00:08:12 +0200

    HttpClientTest.shouldReadFailureStatusCode green

--- src/test/java/no/kristiania/httpclient/HttpClientTest.java
+++ src/test/java/no/kristiania/httpclient/HttpClientTest.java
@@ -2,17 +2,19 @@ package no.kristiania.httpclient;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HttpClientTest {
 
     @Test
-    void shouldReadSuccessStatusCode() {
+    void shouldReadSuccessStatusCode() throws IOException {
         HttpClient httpClient = new HttpClient("urlecho.appspot.com", 80, "/echo?status=200");
         assertEquals(200, httpClient.getResponseCode());
     }
     @Test
-    void shouldReadFailureStatusCode() {
+    void shouldReadFailureStatusCode() throws IOException {
         HttpClient httpClient = new HttpClient("urlecho.appspot.com", 80, "/echo?status=401");
         assertEquals(401, httpClient.getResponseCode());
     }
