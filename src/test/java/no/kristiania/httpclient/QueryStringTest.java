f8243438148e18f874b8f69392ca6e2537dafc7a
Johannes Brodwall <johannes@brodwall.com>
2020-09-01 23:12:48 +0200

    Complete parsing of QueryString (#1)
    Co-authored-by: iaffs <byiaffs@gmail.com>

--- src/test/java/no/kristiania/httpclient/QueryStringTest.java
+++ src/test/java/no/kristiania/httpclient/QueryStringTest.java
@@ -11,4 +11,24 @@ public class QueryStringTest {
         QueryString queryString = new QueryString("status=200");
         assertEquals("200", queryString.getParameter("status"));
     }
+
+    @Test
+    void shouldRetrieveOtherQueryParameter() {
+        QueryString queryString = new QueryString("status=404");
+        assertEquals("404", queryString.getParameter("status"));
+    }
+
+    @Test
+    void shouldRetrieveParameterByName() {
+        QueryString queryString = new QueryString("text=Hello");
+        assertEquals(null, queryString.getParameter("status"));
+        assertEquals("Hello", queryString.getParameter("text"));
+    }
+
+    @Test
+    void shouldHandleMultipleParameters() {
+        QueryString queryString = new QueryString("text=Hello&status=200");
+        assertEquals("200", queryString.getParameter("status"));
+        assertEquals("Hello", queryString.getParameter("text"));
+    }
 }
