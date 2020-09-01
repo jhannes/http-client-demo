0f38e9c82bf3638aa39905b9315bb9c0b5f665f5
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2020-09-01 23:57:59 +0200

    HttpClientTest.shouldReadSuccessStatusCode green; .shouldReadFailureStatusCode red

--- src/test/java/no/kristiania/httpclient/HttpClientTest.java
+++ src/test/java/no/kristiania/httpclient/HttpClientTest.java
@@ -11,4 +11,9 @@ class HttpClientTest {
         HttpClient httpClient = new HttpClient("urlecho.appspot.com", 80, "/echo?status=200");
         assertEquals(200, httpClient.getResponseCode());
     }
+    @Test
+    void shouldReadFailureStatusCode() {
+        HttpClient httpClient = new HttpClient("urlecho.appspot.com", 80, "/echo?status=401");
+        assertEquals(401, httpClient.getResponseCode());
+    }
 }
